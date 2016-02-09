379b732c94468bbec323c46b6de3a5fbaf46d2b0
Alexandre Bléron <alex.bleron@gmail.com>
2016-02-09 17:49:49 +0100

    Painter: minor update

--- examples/simple/glsl/shading_curve.glsl
+++ examples/simple/glsl/shading_curve.glsl
@@ -10,20 +10,30 @@ layout(std140, binding = 1) uniform U1 { vec3 lightPos; };
 
 layout(local_size_x = 16, local_size_y = 16) in;
 layout(binding = 0) uniform sampler2D texNormals;
-layout(binding = 1) uniform sampler2D texShadingTerm;	// same size as the canvas
-layout(binding = 2) uniform sampler2D texMask;	// same size as the canvas
-layout(binding = 3) uniform sampler2D texBaseColor;
+layout(binding = 1) uniform sampler2D texMask;	// same size as the canvas
+layout(binding = 2) uniform sampler2D texBaseColor;
 
 layout(binding = 0, r32ui) coherent uniform uimage1D imgCurveH;
 layout(binding = 1, r32ui) coherent uniform uimage1D imgCurveS;
 layout(binding = 2, r32ui) coherent uniform uimage1D imgCurveV;
 layout(binding = 3, r32ui) coherent uniform uimage1D imgCurveAccum;
 
+vec3 evalNormalMap(vec4 v)
+{
+	return v.xyz * 2.0 - vec3(1.0); 
+}
+
+float shadingTerm(vec4 n)
+{
+	vec3 N = evalNormalMap(n);
+	return dot(N, -lightPos);
+}
+
 void main()
 {
   ivec2 texelCoords = ivec2(gl_GlobalInvocationID.xy);
   vec4 C = texelFetch(texBaseColor, texelCoords, 0);
-  float S = texelFetch(texShadingTerm, texelCoords, 0).x;
+  float S = shadingTerm(texelFetch(texNormals, texelCoords, 0));
   int bin = clamp(int(floor(S * CURVE_SAMPLES)), 0, CURVE_SAMPLES);
 
   vec3 hsv = rgb2hsv(C.xyz);
