1d21cb61a4a7fa088f929a6e78096a356760c0d6
Alexandre Bléron <alex.bleron@gmail.com>
2016-02-18 22:18:54 +0100

    Updated vulkan test

--- examples/vulkan_test/main.cpp
+++ examples/vulkan_test/main.cpp
@@ -28,6 +28,9 @@ const char* getVkPhysicalDeviceTypeString(VkPhysicalDeviceType type) {
   }
 }
 
+constexpr auto kWidth = 800u;
+constexpr auto kHeight = 600u;
+
 int main() {
   GLFWwindow* window;
 
@@ -35,30 +38,36 @@ int main() {
   if (!glfwInit())
     return -1;
 
-  // query vulkan extensions
-  int count;
-  const char** extensions = glfwGetRequiredInstanceExtensions(&count);
+  /////////////////////////////////
+  // query required vulkan extensions
+  // for creating a presentable surface
+  int requiredExtensionCount;
+  const char** requiredExtensionNames =
+      glfwGetRequiredInstanceExtensions(&requiredExtensionCount);
 
-  if (!extensions) {
+  if (!requiredExtensionNames) {
     fmt::print(std::cerr, "glfwGetRequiredInstanceExtensions returned nullptr. "
                           "Cannot create a presentable surface.");
     glfwTerminate();
     return -1;
   }
 
-  for (int i = 0; i < count; ++i)
-	  fmt::print("Extension: {}\n", extensions[i]);
+  for (int i = 0; i < requiredExtensionCount; ++i)
+    fmt::print("Extension: {}\n", requiredExtensionNames[i]);
+
+  /////////////////////////////////
   // Create a vulkan instance
   VkInstance instance = nullptr;
 
   {
     VkInstanceCreateInfo ici = {};
     ici.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
-    ici.enabledExtensionCount = count;
-    ici.ppEnabledExtensionNames = extensions;
+    ici.enabledExtensionCount = requiredExtensionCount;
+    ici.ppEnabledExtensionNames = requiredExtensionNames;
     vkCreateInstance(&ici, nullptr, &instance);
   }
 
+  /////////////////////////////////
   // enumerate physical devices
   uint32_t physDevCount;
   vkEnumeratePhysicalDevices(instance, &physDevCount, nullptr);
@@ -70,7 +79,8 @@ int main() {
   std::vector<VkPhysicalDevice> physDevices(physDevCount);
   vkEnumeratePhysicalDevices(instance, &physDevCount, physDevices.data());
 
-  // dump info
+  /////////////////////////////////
+  // Select a physical device
   VkPhysicalDevice mainPhysDev;
   VkPhysicalDeviceType mainPhysDevType = VK_PHYSICAL_DEVICE_TYPE_MAX_ENUM;
   fmt::print("Vulkan physical devices: \n");
@@ -98,6 +108,11 @@ int main() {
     }
   }
 
+  // Note: the physical device must support VK_KHR_swapchain!
+  // TODO: check it
+
+  /////////////////////////////////
+  // Find which queue families we want
   uint32_t queueFamilyIndex;
   {
     // things
@@ -113,8 +128,12 @@ int main() {
       if (p.queueFlags & VK_QUEUE_GRAPHICS_BIT &&
           p.queueFlags & VK_QUEUE_COMPUTE_BIT &&
           p.queueFlags & VK_QUEUE_TRANSFER_BIT) {
-        queueFamilyIndex = i;
-        break;
+        // this queue must support presentation
+        if (glfwGetPhysicalDevicePresentationSupport(instance, mainPhysDev,
+                                                     i)) {
+          queueFamilyIndex = i;
+          break;
+        }
       }
     }
     if (queueFamilyIndex == queueFamilyPropertyCount) {
@@ -124,42 +143,120 @@ int main() {
     }
   }
 
-  // open window
-  glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-  window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-  if (!window) {
-	  glfwTerminate();
-	  return -1;
-  }
+  // enum extensions
+  /*{
+    uint32_t numExtensions = 0;
+    vkEnumerateInstanceExtensionProperties(nullptr, &numExtensions, nullptr);
+    std::vector<VkExtensionProperties> props(numExtensions);
+    vkEnumerateInstanceExtensionProperties(nullptr, &numExtensions,
+                                           props.data());
+    for (const auto& p : props) {
+      fmt::print("{} (version={})\n", p.extensionName, p.specVersion);
+    }
+  }*/
 
-  // create surface
-  VkSurfaceKHR surface;
-  VkResult err = glfwCreateWindowSurface(instance, window, NULL, &surface);
-  if (err != VK_SUCCESS) {
-	  fmt::print("Could not create a presentable surface\n");
-	  return -1;
-  }
-
-
+  /////////////////////////////////
   // create a vulkan device
   VkDevice device;
 
   {
+	  const char* deviceExts[] = { VK_KHR_SWAPCHAIN_EXTENSION_NAME };
     float queuePriorities[] = {0.0f};
     VkDeviceQueueCreateInfo qci[] = {
         {VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO, nullptr, 0,
          queueFamilyIndex, 1, queuePriorities}};
     VkDeviceCreateInfo dci = {};
     dci.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
-    dci.enabledExtensionCount = count;
-    dci.ppEnabledExtensionNames = extensions;
+    dci.enabledExtensionCount = 1;
+    dci.ppEnabledExtensionNames = deviceExts;
     dci.pQueueCreateInfos = qci;
     dci.queueCreateInfoCount = 1;
 
     auto err = vkCreateDevice(mainPhysDev, &dci, nullptr, &device);
-	fmt::print("vkCreateDevice returned {}\n", err);
+    fmt::print("vkCreateDevice returned {}\n", err);
   }
 
+  /////////////////////////////////
+  // open window
+  glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+  window = glfwCreateWindow(kWidth, kHeight, "Hello World", NULL, NULL);
+  if (!window) {
+    glfwTerminate();
+    return -1;
+  }
+
+  /////////////////////////////////
+  // create surface
+  VkSurfaceKHR surface;
+  VkResult err = glfwCreateWindowSurface(instance, window, NULL, &surface);
+  if (err != VK_SUCCESS) {
+    fmt::print("Could not create a presentable surface\n");
+    return -1;
+  }
+
+  /////////////////////////////////
+  // Query supported swapchain formats
+  VkFormat surfaceFormat;
+  VkColorSpaceKHR surfaceColorSpace;
+  {
+    uint32_t surfaceFormatCount = 0;
+    vkGetPhysicalDeviceSurfaceFormatsKHR(mainPhysDev, surface,
+                                         &surfaceFormatCount, nullptr);
+    std::vector<VkSurfaceFormatKHR> surfaceFormats(surfaceFormatCount);
+    vkGetPhysicalDeviceSurfaceFormatsKHR(
+        mainPhysDev, surface, &surfaceFormatCount, surfaceFormats.data());
+    fmt::print("Supported surface formats:\n");
+    for (const auto& p : surfaceFormats) {
+      fmt::print("- format={}, colorSpace={}\n", p.format, p.colorSpace);
+    }
+    assert(surfaceFormatCount != 0);
+    // just choose the first one...
+    surfaceFormat = surfaceFormats[0].format;
+    surfaceColorSpace = surfaceFormats[0].colorSpace;
+  }
+
+  /////////////////////////////////
+  // create swapchain
+  VkSwapchainKHR swapchain;
+  {
+    uint32_t queueFamilyIndices[] = {queueFamilyIndex};
+    VkSwapchainCreateInfoKHR swapCreateInfo{};
+    swapCreateInfo.sType = VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR;
+    swapCreateInfo.surface = surface;
+    swapCreateInfo.minImageCount = 2;
+    swapCreateInfo.imageFormat = surfaceFormat;
+    swapCreateInfo.imageColorSpace = surfaceColorSpace;
+    swapCreateInfo.imageExtent = VkExtent2D{kWidth, kHeight};
+    swapCreateInfo.queueFamilyIndexCount = 1;
+    swapCreateInfo.pQueueFamilyIndices = queueFamilyIndices;
+    swapCreateInfo.imageArrayLayers = 1;
+	swapCreateInfo.imageUsage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT;
+    swapCreateInfo.compositeAlpha =
+        VK_COMPOSITE_ALPHA_OPAQUE_BIT_KHR; // whatever that means
+    swapCreateInfo.presentMode = VK_PRESENT_MODE_IMMEDIATE_KHR;       // same
+    swapCreateInfo.imageSharingMode = VK_SHARING_MODE_EXCLUSIVE; // idem
+    swapCreateInfo.preTransform =
+        VK_SURFACE_TRANSFORM_IDENTITY_BIT_KHR; // ibidem
+	auto err = vkCreateSwapchainKHR(device, &swapCreateInfo, NULL, &swapchain);
+	fmt::print("vkCreateSwapchainKHR returned {}\n", err);
+  }
+
+  // Again this should be properly enumerated
+  /*VkImage images[4];
+  uint32_t swapCount;
+  vkGetSwapchainImagesKHR(dev, swap, &swapCount, images);
+
+  // Synchronisation is needed here!
+  uint32_t currentSwapImage;
+  vkAcquireNextImageKHR(dev, swap, UINT64_MAX, presentCompleteSemaphore, NULL,
+                        &currentSwapImage);
+
+  // pass appropriate creation info to create view of image
+  VkImageView backbufferView;
+  vkCreateImageView(dev, &backbufferViewCreateInfo, NULL, &backbufferView);*/
+
+  /*VkQueue queue;
+  vkGetDeviceQueue(dev, 0, 0, &queue);*/
 
   /* Loop until the user closes the window */
   while (!glfwWindowShouldClose(window)) {
@@ -173,6 +270,5 @@ int main() {
   }
 
   glfwTerminate();
-  return 0;
   return 0;
 }
