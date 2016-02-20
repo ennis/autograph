bc4f4925820a6aa06c5730e5f91acca64498f7f8
Alexandre Bléron <alex.bleron@gmail.com>
2016-02-20 17:53:18 +0100

    Vulkan example progress
    Clearing an image of the swapchain seems to work

--- examples/vulkan_test/main.cpp
+++ examples/vulkan_test/main.cpp
@@ -31,6 +31,14 @@ const char* getVkPhysicalDeviceTypeString(VkPhysicalDeviceType type) {
 constexpr auto kWidth = 800u;
 constexpr auto kHeight = 600u;
 
+VkSemaphore createSemaphore(VkDevice device) {
+  VkSemaphore semaphore;
+  VkSemaphoreCreateInfo semaphoreCreateInfo = {
+      VK_STRUCTURE_TYPE_SEMAPHORE_CREATE_INFO, nullptr, 0};
+  vkCreateSemaphore(device, &semaphoreCreateInfo, nullptr, &semaphore);
+  return semaphore;
+}
+
 int main() {
   GLFWwindow* window;
 
@@ -160,7 +168,7 @@ int main() {
   VkDevice device;
 
   {
-	  const char* deviceExts[] = { VK_KHR_SWAPCHAIN_EXTENSION_NAME };
+    const char* deviceExts[] = {VK_KHR_SWAPCHAIN_EXTENSION_NAME};
     float queuePriorities[] = {0.0f};
     VkDeviceQueueCreateInfo qci[] = {
         {VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO, nullptr, 0,
@@ -230,16 +238,89 @@ int main() {
     swapCreateInfo.queueFamilyIndexCount = 1;
     swapCreateInfo.pQueueFamilyIndices = queueFamilyIndices;
     swapCreateInfo.imageArrayLayers = 1;
-	swapCreateInfo.imageUsage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT;
+    swapCreateInfo.imageUsage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT;
     swapCreateInfo.compositeAlpha =
         VK_COMPOSITE_ALPHA_OPAQUE_BIT_KHR; // whatever that means
-    swapCreateInfo.presentMode = VK_PRESENT_MODE_IMMEDIATE_KHR;       // same
+    swapCreateInfo.presentMode = VK_PRESENT_MODE_IMMEDIATE_KHR;  // same
     swapCreateInfo.imageSharingMode = VK_SHARING_MODE_EXCLUSIVE; // idem
     swapCreateInfo.preTransform =
         VK_SURFACE_TRANSFORM_IDENTITY_BIT_KHR; // ibidem
-	auto err = vkCreateSwapchainKHR(device, &swapCreateInfo, NULL, &swapchain);
-	fmt::print("vkCreateSwapchainKHR returned {}\n", err);
+    auto err = vkCreateSwapchainKHR(device, &swapCreateInfo, NULL, &swapchain);
+    fmt::print("vkCreateSwapchainKHR returned {}\n", err);
   }
+
+  /////////////////////////////////
+  // Get the images in the swapchain
+  // and create render target views
+  std::vector<VkImage> swapchainImages;
+  std::vector<VkImageView> swapchainImageViews;
+  {
+    uint32_t swapchainImageCount = 0;
+    vkGetSwapchainImagesKHR(device, swapchain, &swapchainImageCount, nullptr);
+    swapchainImages.resize(swapchainImageCount);
+    swapchainImageViews.resize(swapchainImageCount);
+    vkGetSwapchainImagesKHR(device, swapchain, &swapchainImageCount,
+                            swapchainImages.data());
+
+    for (uint32_t i = 0; i < swapchainImageCount; ++i) {
+      VkImageViewCreateInfo viewCreateInfo = {};
+      viewCreateInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+      viewCreateInfo.format = surfaceFormat;
+      viewCreateInfo.image = swapchainImages[i];
+      viewCreateInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
+      viewCreateInfo.subresourceRange = VkImageSubresourceRange{
+          VK_IMAGE_ASPECT_COLOR_BIT, // aspectMask
+          0,                         // baseMipLevel
+          1,                         // levelCount
+          0,                         // baseArrayLayer
+          1                          // layerCount
+      };
+      viewCreateInfo.components =
+          VkComponentMapping{VK_COMPONENT_SWIZZLE_R, VK_COMPONENT_SWIZZLE_G,
+                             VK_COMPONENT_SWIZZLE_B, VK_COMPONENT_SWIZZLE_A};
+      vkCreateImageView(device, &viewCreateInfo, nullptr,
+                        &swapchainImageViews[i]);
+    }
+  }
+
+  /////////////////////////////////
+  // Create a command pool
+  VkCommandPool commandPool;
+  {
+    VkCommandPoolCreateInfo commandPoolCreateInfo = {
+        VK_STRUCTURE_TYPE_COMMAND_POOL_CREATE_INFO, nullptr, 0, queueFamilyIndex
+        // nothing interesting
+    };
+
+    vkCreateCommandPool(device, &commandPoolCreateInfo, NULL, &commandPool);
+  }
+
+  /////////////////////////////////
+  // Create a command buffer for each
+  std::vector<VkCommandBuffer> commandBuffers(swapchainImages.size(), nullptr);
+  // fences associated with the completion of the command buffer submissions to
+  // the queue
+  std::vector<VkFence> submitFences(swapchainImages.size(), nullptr);
+  std::vector<VkSemaphore> submitSemaphores(swapchainImages.size(), nullptr);
+
+  for (auto& fence : submitFences) {
+    VkFenceCreateInfo fenceCreateInfo = {VK_STRUCTURE_TYPE_FENCE_CREATE_INFO,
+                                         nullptr};
+    vkCreateFence(device, &fenceCreateInfo, nullptr, &fence);
+  }
+
+  /*{
+    VkCommandBufferAllocateInfo commandBufferAllocInfo = {
+        VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO, nullptr, commandPool,
+        VK_COMMAND_BUFFER_LEVEL_PRIMARY, 1};
+
+    vkAllocateCommandBuffers(device, &commandBufferAllocInfo, &commandBuffer);
+  }*/
+
+  /////////////////////////////////
+  // get device queue
+  VkQueue queue;
+  vkGetDeviceQueue(device, 0, 0, &queue);
 
   // Again this should be properly enumerated
   /*VkImage images[4];
@@ -258,12 +339,126 @@ int main() {
   /*VkQueue queue;
   vkGetDeviceQueue(dev, 0, 0, &queue);*/
 
+  // create a command pool (one per thread, in theory)
+  // create a command buffer
+  // begin render loop
+  //   get target swapchain image (synchronize)
+  //   transition from present state to color attachement state
+  //   begin command buffer
+  //   end command buffer
+  //   submit command buffer to queue
+  //   reset (clear) command buffer
+  //   transition image to present state
+  //   present image
+  // end render loop
+  //
+
   /* Loop until the user closes the window */
   while (!glfwWindowShouldClose(window)) {
     /* Render here */
 
-    /* Swap front and back buffers */
-    glfwSwapBuffers(window);
+    VkImage swapImage;
+    VkImageView swapImageView;
+
+    uint32_t currentSwapImageIndex;
+    vkAcquireNextImageKHR(device, swapchain, UINT64_MAX, nullptr, nullptr,
+                          &currentSwapImageIndex);
+
+	swapImage = swapchainImages[currentSwapImageIndex];
+	swapImageView = swapchainImageViews[currentSwapImageIndex];
+
+    // allocate a command buffer, or re-use existing
+    VkCommandBuffer cmdBuf = nullptr;
+    if (!commandBuffers[currentSwapImageIndex]) {
+      VkCommandBufferAllocateInfo commandBufferAllocInfo = {
+          VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO, nullptr, commandPool,
+          VK_COMMAND_BUFFER_LEVEL_PRIMARY, 1};
+
+      vkAllocateCommandBuffers(device, &commandBufferAllocInfo, &cmdBuf);
+      commandBuffers[currentSwapImageIndex] = cmdBuf;
+    } else {
+      cmdBuf = commandBuffers[currentSwapImageIndex];
+      vkResetCommandBuffer(cmdBuf, 0);
+    }
+
+    // begin recording
+    VkCommandBufferBeginInfo cmdBufBeginInfo = {
+        VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO, nullptr, 0, nullptr};
+
+    vkBeginCommandBuffer(cmdBuf, &cmdBufBeginInfo);
+
+    const VkImageMemoryBarrier acquireImageBarrier = {
+        VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER,   // sType
+        NULL,                                     // pNext
+        VK_ACCESS_MEMORY_READ_BIT,                // srcAccessMask
+        VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT,     // dstAccessMask
+        VK_IMAGE_LAYOUT_PRESENT_SRC_KHR,          // oldLayout
+        VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL, // newLayout
+        queueFamilyIndex,                         // srcQueueFamilyIndex
+        queueFamilyIndex,                         // dstQueueFamilyIndex
+        swapImage,                                // image
+        VkImageSubresourceRange{VK_IMAGE_ASPECT_COLOR_BIT, 0, 1, 0, 1}};
+
+    vkCmdPipelineBarrier(cmdBuf, VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT,
+                         VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT,
+                         VK_FALSE, 0, nullptr, 0, nullptr, 1,
+                         &acquireImageBarrier);
+
+	// draw here?
+	
+	VkClearColorValue clearColorValue;
+	clearColorValue.float32[0] = 0.0f;
+	clearColorValue.float32[1] = 1.0f;
+	clearColorValue.float32[2] = 0.0f;
+	clearColorValue.float32[3] = 0.0f;
+	VkImageSubresourceRange clearRange = { VK_IMAGE_ASPECT_COLOR_BIT, 0, 1, 0, 1 };
+	vkCmdClearColorImage(cmdBuf, swapImage, VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL, &clearColorValue, 1, &clearRange);
+
+    const VkImageMemoryBarrier presentImageBarrier = {
+        VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER,   // sType
+        NULL,                                     // pNext
+        VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT,     // srcAccessMask
+        VK_ACCESS_MEMORY_READ_BIT,                // dstAccessMask
+        VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL, // oldLayout
+        VK_IMAGE_LAYOUT_PRESENT_SRC_KHR,          // newLayout
+        queueFamilyIndex,                         // srcQueueFamilyIndex
+        queueFamilyIndex,                         // dstQueueFamilyIndex
+        swapImage,                                // image
+        VkImageSubresourceRange{VK_IMAGE_ASPECT_COLOR_BIT, 0, 1, 0, 1}};
+
+    vkCmdPipelineBarrier(cmdBuf, VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT,
+                         VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT,
+                         VK_FALSE, 0, nullptr, 0, nullptr, 1,
+                         &presentImageBarrier);
+
+    vkEndCommandBuffer(cmdBuf);
+
+    if (submitSemaphores[currentSwapImageIndex])
+      vkDestroySemaphore(device, submitSemaphores[currentSwapImageIndex],
+                         nullptr);
+    submitSemaphores[currentSwapImageIndex] = createSemaphore(device);
+
+    VkSubmitInfo submitInfo = {
+        VK_STRUCTURE_TYPE_SUBMIT_INFO, nullptr,
+        // wait on nothing?
+        // should wait on completion of the previous queue submission
+        0, nullptr, nullptr, 1, &cmdBuf, 1,
+        &submitSemaphores[currentSwapImageIndex]};
+
+    vkQueueSubmit(queue, 1, &submitInfo, nullptr);
+
+    VkResult presentResult;
+    // sync on completion of previous command
+    VkPresentInfoKHR presentInfo = {VK_STRUCTURE_TYPE_PRESENT_INFO_KHR,
+                                    nullptr,
+                                    1,
+                                    &submitSemaphores[currentSwapImageIndex],
+                                    1,
+                                    &swapchain,
+                                    &currentSwapImageIndex,
+                                    &presentResult};
+
+    vkQueuePresentKHR(queue, &presentInfo);
 
     /* Poll for and process events */
     glfwPollEvents();
